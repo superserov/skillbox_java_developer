7931cb0c361f66e62eed962b2ec780f9e7fb7c26
Sergey Serov <superserov@inbox.ru>
2024-04-20 16:19:13 +0300

    first commit

---
+++ Cat/src/Cat.java
@@ -0,0 +1,67 @@
+import java.util.stream.IntStream;
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public void pee()
+    {
+        weight = weight - 300;
+        System.out.println("Oh yeah");
+    }
+
+    public void meow()
+    {
+        weight = weight - 100;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+    public Double getFeedWeight()
+    {
+        Double result = weight - originWeight;
+        return (result < 0) ? 0 : result;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
---
+++ Cat/src/Loader.java
@@ -0,0 +1,32 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat murka = new Cat();
+        System.out.println("Murka weight: " + murka.getWeight());
+        System.out.println("Murka status: " + murka.getStatus());
+
+        while (!murka.getStatus().equals("Dead")) {
+            murka.meow();
+        }
+        System.out.println("Murka weight: " + murka.getWeight());
+        System.out.println("Murka status: " + murka.getStatus());
+
+        Cat vasya = new Cat();
+        System.out.println("Vasya weight: " + vasya.getWeight());
+        System.out.println("Vasya status: " + vasya.getStatus());
+        while (!vasya.getStatus().equals("Exploded")) {
+            vasya.feed(100.00);
+            System.out.println("Vasya weight: " + vasya.getWeight());
+        }
+        System.out.println("Vasya status: " + vasya.getStatus());
+        System.out.println("Vasya feed weight: " + vasya.getFeedWeight());
+
+        Cat felix = new Cat();
+        System.out.println("Felix weight: " + felix.getWeight());
+        felix.pee();
+        System.out.println("Felix weight: " + felix.getWeight());
+
+    }
+}
